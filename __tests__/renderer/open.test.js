6e82b58cba1dce63543b7f6b6d947b276c6edca4
nerjs <karifan.stap@gmail.com>
2019-06-29 02:28:10 +0300

    parent init/open tests

---
+++ __tests__/renderer/open.test.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const { remote: { getCurrentWindow }} = require('electron')
+const EM = require('../../lib/electron_modals')
+
+
+
+const template = path.join(__dirname, '..', 'common', 'open_test_template.html')
+
+const {
+    TEMPLATE_IS_REQUIRED_MESS
+} = require('../../lib')
+
+describe('Open win', () => {
+
+    it('Parent', async () => {
+        let em;
+        em = new EM('test')
+
+        await expect(em.open()).rejects.toThrow(TEMPLATE_IS_REQUIRED_MESS)
+
+
+        em = new EM('test', {
+            template, 
+            winOptions: {
+                width: 200, 
+                transparent: true,
+                center: false
+            }
+        })
+
+        await em.open({
+                height: 201
+            }, {}, {
+                x: 202,
+                y: 203
+            }) 
+
+        const [ width, height ] = em.win.getSize() 
+        const [ x, y ] = em.win.getPosition()
+
+
+
+        expect(width).toEqual(200);
+        expect(height).toEqual(201);
+        expect(x).toEqual(202);
+        expect(y).toEqual(203);
+        expect(em.win.isModal()).toEqual(false);
+
+        em.win.close()
+        
+        
+        em = new EM('test', {
+            template,
+            modal: true,
+            winOptions: {
+                width: 200, 
+                transparent: true,
+                center: false
+            }
+        })
+
+        await em.open()
+
+
+        expect(em.win.isModal()).toEqual(true);
+        expect(em.win.getParentWindow()).toEqual(getCurrentWindow());
+    });
+});
