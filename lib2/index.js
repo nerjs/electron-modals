da7c95c64f54868daed7f4d4eef724d7fa1f452f
nerjs <karifan.stap@gmail.com>
2019-06-30 18:28:15 +0300

    next version

--- lib2/index.js
+++
@@ -1,8 +0,0 @@
-exports.ElectronModals = require('./electron_modals')
-exports.ChildElectronModals = require('./child_electron_modals')
-
-const constants = require('./utils/constants') 
-
-Object.keys(constants).forEach(key => {
-    exports[key] = constants[key]
-})
